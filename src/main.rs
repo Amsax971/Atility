4a3a66b327a82f3fac1f183c9ce7f1cff91016ee
Amsax971 <Amsax971>
2024-01-14 18:29:55 +0000

    copy file to /backup (not pushed here) and makes verification

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::fs;
+use std::env::args;
+use std::process;
+use std::io::*;
+
+struct Extract;
+
+impl Extract {
+    fn extract() -> Result<()> {
+        let argc: Vec<String> = args().collect();
+
+        if argc[1] == "-h" {
+            println!("will do this later");
+            process::exit(1)
+        } else if argc[1] == "--help" {
+            println!("will do this later");
+            process::exit(1)
+        }
+
+        let file = &argc[1];
+
+        // let metadata = metadata(file)?;
+
+        let mut name: Vec<&str>  = file.split('.').collect();
+
+        if name[1] != "apk" {
+            println!("the file that you provided isn't a Android Package, please provide a .apk file");
+            process::exit(1)
+        }
+
+        name[1] = ".zip";
+
+        let newname = String::from(name[0]) + name[1];
+
+        println!("{}", newname);
+
+        let backupath = String::from("./backup/") + file;
+
+        fs::copy(file, backupath)?;
+
+        Ok(())
+
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _ext = Extract::extract();
 }
